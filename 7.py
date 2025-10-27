e20b7fae2e10731d81588143fad903464a63ea0c
cxk916794 <160753830+cxk916794@users.noreply.github.com>
2025-10-27 15:03:17 +0800

    Add files via upload

--- 7.py
+++ 7.py
@@ -1,71 +1,66 @@
 import streamlit as st
-
-st.set_page_config(page_title="干员档案", page_icon="", layout="wide")
-
-import streamlit as st
-
-st.subheader('干员信息')
-name = st.text_input('姓名', autocomplete='name')
-
-st.text_input('所属国家')
-
-st.text_input('兵种')
-
-st.text_input('联系方式')
-
-import streamlit as st
-from datetime import datetime, timedelta
-
-# value参数默认为None，初始状态为今天
-date = st.date_input("出生日期", value=None)
-
-def my_format_func(option):
-    return f'选{option}'
-
-st.write('性别')
-# 设置标签为“hidden”
-# 设置水平排列
-lunch = st.radio(
-    '你的性别是什么？',
-    ['男', '女', '沃尔玛购物袋'],
-    horizontal=True,
-    label_visibility='hidden'
-)
-
-city = st.selectbox('学历：', ['幼儿园', '小学', '初中','高中','大学'], format_func=my_format_func, index=2)
-
-city = st.selectbox('喜欢的食物：', ['扣肉拿铁', '爆辣冰淇淋', '哈基米南北绿豆','白丝乌鲁鲁'], format_func=my_format_func, index=2)
-
-options_2 = st.multiselect(
-    '擅长的技能',
-    ['蜂巢烟幕', '蜂巢科技烟雾弹', '治疗激素枪', '高效救援', '乞讨'],
-    max_selections=5)
-
-import streamlit as st
-from datetime import datetime, time
-
-age = st.slider('作战时间', 0, 100)
-st.write("以作战 ", age, '年')
-
-start_color, end_color = st.select_slider(
-    '期望薪资范围',
-    options=['1', '5000', '10000', '15000', '20000', '25000', '30000'],
-    value=('1', '15000'))
-st.write('你期望的薪资介于', start_color, '和', end_color, '之间')
-
-init_text = "罗伊出生于治安混乱的小镇，青少年时期曾陷入街头帮派活动，成年后参军并师从老军医学习急救技术。因战场救援表现突出获得进修机会，后加入特种部队支援群。其家庭由妻子和八岁女儿组成，所有收入均寄回家乡。"
-st.text_area(label='个人简介：', value=init_text,
-            height=200, max_chars=200)
-
-w2 = st.time_input("活跃时间", time(2,00))
-st.write("你选择时间2是:", w2)
-
-import streamlit as st
 import pandas as pd
 from io import StringIO
-st.header('上传个人照片')
-uploaded_file = st.file_uploader("选择一个CSV文件")
-if uploaded_file is not None:
-    bytes_date = uploaded_file.getvalue()
-    st.subheader('直接展示字节数据')
-    st.write(bytes_data)
+from io import BytesIO
+from datetime import datetime, time
+st.set_page_config(page_title="个人简历生成器", page_icon="", layout="wide")
+st.title('个人简历生成器')
+c1, c2 = st.columns([1,2])
+with c1:
+    st.subheader('特战干员资料')
+    st.markdown('***')
+    def my_format_func(option):
+        return f'{option}'
+    name = st.text_input('姓名', autocomplete='name')
+    size = st.radio(
+    '性别',
+    ['男', '女', '沃尔玛购物袋'],
+    horizontal=True,
+    label_visibility='hidden',)
+    learn = st.selectbox('国籍：',['英国', '欧洲', '日本','俄罗斯','哈基米国'], format_func=my_format_func, index=2)
+    languages = st.multiselect('精通语言：',['俄语', '英语', '法语','日语','哈基米语','火星文'],format_func=my_format_func)
+    job = st.text_input('应聘职位', autocomplete='job')
+    tl = st.text_input('干员职业', autocomplete='tl')
+    email = st.text_input('邮箱', autocomplete='email') 
+    age = st.number_input("年龄", min_value=1, max_value=130, value=10)
+    date = st.date_input("生日", value=None)
+    w3 = st.time_input("活跃时间段", datetime(2019, 7, 6, 21, 15))
+    age = st.slider('工龄', 0, 50)
+    values = st.slider(
+    '期望薪资',
+    0, 100000,(10000, 20000))
+    t5 = st.text_area(label='个人简介：', placeholder='请简单描述自己')
+    uploaded_file = st.file_uploader("上传个人照片", type=["jpg", "png", "jpeg"])
+    if uploaded_file is not None:
+        bytes_data = uploaded_file.getvalue()
+        
+        
+    
+    
+with c2:
+    st.subheader('实时简历预览')
+    st.markdown('***')
+    a1, a2, a3 = st.columns(3)
+    with a1:
+        if uploaded_file is not None:
+            bytes_data = uploaded_file.getvalue()
+            st.image(bytes_data, caption="本人照片", width=250)
+        
+    with a2:
+        st.title(name)
+        st.write('**国籍**:',learn)
+        st.write(f'**性别**:{size}')
+        st.write('**邮箱**:', email)
+        st.write('**干员职业**:', tl)
+        st.write('**年龄**:', age)
+    with a3:
+        st.write('**应聘职位**:', job)
+        st.write("**生日**:", date)
+        st.write("**活跃时间段**:", w3)
+        st.write('**期望薪资**:', values)
+        st.write('**精通语言**:', languages)
+        st.write("**工龄**: ", age, '年')
+        
+    st.markdown('***')
+    st.title('**个人简介**')
+    st.write(t5)
